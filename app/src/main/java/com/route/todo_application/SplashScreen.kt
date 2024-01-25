8a07c05d74945354e19bf5a9696b57940e89798c
sherif Abdelrhman <sellkady038@gmail.com>
2024-01-25 23:41:44 +0200

    Home Screen Xml setup finish.

--- app/src/main/java/com/route/todo_application/SplashScreen.kt
+++ app/src/main/java/com/route/todo_application/SplashScreen.kt
@@ -1,11 +1,22 @@
 package com.route.todo_application
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler(mainLooper).postDelayed(object : Runnable{
+            override fun run() {
+               val intent = Intent(this@SplashScreen,HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        },2000)
     }
 }
