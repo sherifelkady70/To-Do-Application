eef72ee6cb6bd345403fe25036413ac422f53dbc
sherif Abdelrhman <sellkady038@gmail.com>
2024-07-20 23:33:26 +0300

    create todo Repository object to inject in use case.

--- app/src/main/java/com/route/todo_application/data/datasource/di/DatasourceModule.kt
+++ app/src/main/java/com/route/todo_application/data/datasource/di/DatasourceModule.kt
@@ -2,6 +2,7 @@ package com.route.todo_application.data.datasource.di
 
 import com.route.todo_application.data.datasource.offline_datasource.TodoOfflineDatasource
 import com.route.todo_application.data.datasource.offline_datasource.TodoOfflineDatasourceImpl
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -10,7 +11,7 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class DatasourceModule {
-    @Provides
+    @Binds
     abstract fun provideOfflineDatasource(impl:TodoOfflineDatasourceImpl)
     :TodoOfflineDatasource
 }
---
+++ app/src/main/java/com/route/todo_application/data/repository/di/RepositoryModule.kt
@@ -0,0 +1,18 @@
+package com.route.todo_application.data.repository.di
+
+import com.route.todo_application.data.repository.TodoRepositoryImpl
+import com.route.todo_application.domain.repository.TodoRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule  {
+
+    @Binds
+    abstract fun provideTodoRepository(impl:TodoRepository)
+    :TodoRepositoryImpl
+}
