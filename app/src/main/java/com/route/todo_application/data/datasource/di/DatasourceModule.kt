be943ddaf4d8dc65c29da41c04ef9388b30a3cac
sherif Abdelrhman <sellkady038@gmail.com>
2024-07-20 23:16:31 +0300

    create offline datasource object to inject in repo.

---
+++ app/src/main/java/com/route/todo_application/data/datasource/di/DatasourceModule.kt
@@ -0,0 +1,17 @@
+package com.route.todo_application.data.datasource.di
+
+import com.route.todo_application.data.datasource.offline_datasource.TodoOfflineDatasource
+import com.route.todo_application.data.datasource.offline_datasource.TodoOfflineDatasourceImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DatasourceModule {
+    @Provides
+    abstract fun provideOfflineDatasource(impl:TodoOfflineDatasourceImpl)
+    :TodoOfflineDatasource
+}
